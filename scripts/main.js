3b0aed295be0f6ddeab4e0161266b087dbb1ca1c
David Perez <dperezval08@gmail.com>
2019-09-09 10:58:01 -0500

    Actualicé la UI del home.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,11 @@
-let personajesHome = [493, 493, 493];
+let personajesHome = [0, 0, 0];
 for(let i = 0; i<personajesHome.length; i++){
   let random = Math.floor(Math.random() * ((493+1) -1) + 1);
   personajesHome[i] =  random
-  console.log(random);
 }
-//---------------------------------------------------------------------//
 
 const elemento = document.querySelector('#contenedor')
+
 //----------Consumiendo API con Async/Await----------//
 async function onLoad(id){
   async function getData(url){
@@ -16,19 +15,11 @@ async function onLoad(id){
   }
   try{
     const datos = await getData(`https://rickandmortyapi.com/api/character/${id}`);
-    console.log(datos);
     const bloqueEnHTML =
       `<div class="item">
         <img src="${datos.image}" alt="Character img">
         <p>
-          <label for="">Name: </label>
           <span>${datos.name}</span><br>
-          <label for="">Status: </label>
-          <span>${datos.status}</span><br>
-          <label for="">Species: </label>
-          <span>${datos.species}</span><br>
-          <label for="">Gender: </label>
-          <span>${datos.gender}</span><br>
         </p>
       </div>`
     const html = document.implementation.createHTMLDocument() //creamos un document virtual
@@ -39,6 +30,5 @@ async function onLoad(id){
     console.log('Algo salio mal');
   }
 }
-let resultadosAsyncAwait = personajesHome.map(onLoad)
-console.log(resultadosAsyncAwait)
-//---------------------------------------------------------------------
+personajesHome.map(onLoad)
+//----------------------------------------------------//
